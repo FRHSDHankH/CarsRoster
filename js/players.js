c52f649fe724e04b2d0c2152b852056bbbf49df6
FRHSDHankH <526hhabashy@frhsd.com>
2025-05-15 10:08:16 -0400

    Started Files and some stuffs idek what is happening i miss 3 days kilgore help.

---
+++ js/players.js
@@ -0,0 +1,182 @@
+const players = [
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  },
+  {
+    make: '',
+    model: '',
+    funFact: '',
+    photo: 'https://picsum.photos/300/300'
+  }
+]
---
+++ js/roster.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.getElementById('roster-grid')
+    const render = list => {
+        grid.innerHTML = ''
+        list.forEach(p => {
+            const col = document.createElement('div')
+            col.className = 'col-6 col-lg-2'
+            col.innerHTML = `
+             <div class="card h-100 shadow-sm">
+                    <img src="${p.photo}" alt="${p.firstName}'s Beautiful Face" class="card-img-top">
+                    <div class="card-body text-center">
+                    <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
+                    <div>${p.side}</div>
+                    </div>
+            </div>
+             `
+             grid.appendChild(col)
+        })
+    }
+
+    render(players)
+})
+
+
+
