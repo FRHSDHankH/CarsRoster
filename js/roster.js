189ac49329f233048983dfb9a6a246020409d2cb
FRHSDHankH <526hhabashy@frhsd.com>
2025-05-20 09:28:39 -0400

    Actually Started frfr

--- js/roster.js
+++ js/roster.js
@@ -1,25 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.getElementById('roster-grid')
-    const render = list => {
-        grid.innerHTML = ''
-        list.forEach(p => {
-            const col = document.createElement('div')
-            col.className = 'col-6 col-lg-2'
-            col.innerHTML = `
-             <div class="card h-100 shadow-sm">
-                    <img src="${p.photo}" alt="${p.firstName}'s Beautiful Face" class="card-img-top">
-                    <div class="card-body text-center">
-                    <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
-                    <div>${p.side}</div>
-                    </div>
-            </div>
-             `
-             grid.appendChild(col)
-        })
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  const carousel = document.getElementById("addItems");
+  const render = (players) => {
+    carousel.innerHTML = "";
+    players.forEach((p) => {
+      const item = document.createElement("div");
+      item.className = "carousel-item";
+      item.innerHTML = `
+        <div class="card" style="width: 18rem;">
+          <img src="${p.photo}" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">${p.make} ${p.model}</h5>
+            <btn onclick="" class="btn btn-primary">More Info</btn>
+          </div>
+        </div>
+      `;
+      carousel.appendChild(item);
+    });
+  };
 
-    render(players)
-})
-
-
-
+  render(players);
+});
