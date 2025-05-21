92a2f4b9f0d066553073fd677ffa83f23ebe9e54
FRHSDHankH <526hhabashy@frhsd.com>
2025-05-21 10:34:07 -0400

    All i need is images now

--- js/roster.js
+++ js/roster.js
@@ -2,18 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const carousel = document.getElementById("addItems");
   const render = (players) => {
     carousel.innerHTML = "";
-    players.forEach((p) => {
+    players.forEach((p, index) => {
       const item = document.createElement("div");
-      item.className = "carousel-item";
+      item.className = "carousel-item text-center" + (index === 0 ? " active" : "");
       item.innerHTML = `
-        <div class="card" style="width: 18rem;">
-          <img src="${p.photo}" class="card-img-top">
+        <div class="card mx-auto bg-transparent text-white font border-0 fadeIn" style="width: 50vw;">
+          <img src="${p.photo}" class="card-img-top img-fluid mx-auto w-50">
           <div class="card-body">
-            <h5 class="card-title">${p.make} ${p.model}</h5>
-            <btn onclick="" class="btn btn-primary">More Info</btn>
+            <p class="card-title display-4">${p.make} ${p.model}</p>
+            <button onclick="" class="btn btn-transparent text-white border border-1 border-white text-center display-6 scale">More Info</button>
           </div>
         </div>
-      `;
+  `;
       carousel.appendChild(item);
     });
   };
