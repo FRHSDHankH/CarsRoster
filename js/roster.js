6b9160286051e3ab3f27231cf912fe51bbe057dd
FRHSDHankH <526hhabashy@frhsd.com>
2025-05-30 09:07:04 -0400

    Kilgore took 30 mins to fix it it work now

--- js/roster.js
+++ js/roster.js
@@ -1,29 +1,30 @@
 function show(make) {
-  const car = document.getElementById(make)
-  car.classList.add('d-block')
-  car.classList.remove('d-none')
-  document.getElementById('unHideFr').classList.remove('d-none')
-  document.getElementById('hideFr').classList.add('d-none')
+  const car = document.getElementById(make);
+  car.classList.add("d-block");
+  car.classList.remove("d-none");
+  document.getElementById("unHideFr").classList.remove("d-none");
+  document.getElementById("hideFr").classList.add("d-none");
 }
 
 function close(make) {
-  console.log('i work')
-  const car = document.getElementById(make)
-  car.classList.remove('d-block')
-  car.classList.add('d-none')
-  document.getElementById('unHideFr').classList.add('d-none')
-  document.getElementById('hideFr').classList.remove('d-none')
+  console.log("i work");
+  const car = document.getElementById(make);
+  car.classList.remove("d-block");
+  car.classList.add("d-none");
+  document.getElementById("unHideFr").classList.add("d-none");
+  document.getElementById("hideFr").classList.remove("d-none");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const carousel = document.getElementById("addItems");
-  const modals = document.getElementById('modals')
+  const modals = document.getElementById("modals");
   const render = (players) => {
     carousel.innerHTML = "";
     modals.innerHTML = "";
     players.forEach((p, index) => {
       const item = document.createElement("div");
-      item.className = "carousel-item text-center" + (index === 0 ? " active" : "");
+      item.className =
+        "carousel-item text-center" + (index === 0 ? " active" : "");
       item.innerHTML = `
         <div class="card mx-auto bg-transparent text-white font border-0 fadeIn" style="width: 50vw;">
           <img src="${p.photo}" class="card-img-top img-fluid mx-auto w-50">
@@ -35,17 +36,21 @@ document.addEventListener("DOMContentLoaded", () => {
       carousel.appendChild(item);
     });
 
-    players.forEach(p => {
-      const modal = document.createElement('div')
-      modal.className = 'rounded w-75 font display-4 text-white text-center mx-auto border border-1 border-white bg-transparent d-none fadeIn p-5'
-      modal.id = p.idName
+    players.forEach((p) => {
+      const modal = document.createElement("div");
+      modal.className =
+        "rounded w-75 font display-4 text-white text-center mx-auto border border-1 border-white bg-transparent d-none fadeIn p-5";
+      modal.id = p.idName;
       modal.innerHTML = `
-        ${p.make} ${p.model} Fun Fact:<br>
-        <span class="fs-4">
-          ${p.funFact}
-        </span><br>
-        <button onclick="close('${p.idName}')" class='fs-2 text-white bg-transparent border border-1 border-white scale rounded text-center p-3'>Close</button>
-      `
+    ${p.make} ${p.model} Fun Fact:<br>
+    <span class="fs-4">${p.funFact}</span><br>
+    <button class='fs-2 text-white bg-transparent border border-1 border-white scale rounded text-center p-3'>Close</button>
+  `;
+
+      // Attach event listener to the button
+      const button = modal.querySelector("button");
+      button.addEventListener("click", () => close(p.idName));
+
       modals.appendChild(modal);
     });
   };
